e3e93d3da29295d98110619345bb46ebf2e22622
Cristian <cb2onix@gmail.com>
2025-04-14 21:19:57 -0300

    feat: meteors upd

--- main.py
+++ main.py
@@ -8,16 +8,24 @@ HEIGHT = 600
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Lluvia Espacial")
 
+#Colores
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
+# Jugador
 player_width = 50
 player_height = 50
 
+# Meteoritos
+meteor_width = 30
+meteor_height = 30
+meteors = []
+
+
 player = pygame.Rect(WIDTH // 2 - player_width // 2,
                      HEIGHT - player_height - 10, player_width, player_height)
-#Reloj para controlar FPS
+#Reloj para control de FPS
 clock  = pygame.time.Clock()
 
 running = True
@@ -36,8 +44,22 @@ while running:
     if keys[pygame.K_DOWN] and player.bottom > 0:
         player.y += 5
 
+    # Generar meteoritos
+    if len(meteors) < 7:
+        meteor = pygame.Rect(random.randint(0, WIDTH - meteor_width),
+                             0, meteor_width, meteor_height)
+        meteors.append(meteor)
+
+    # Mover meteoritos
+    for meteor in meteors:
+        meteor.y += 5
+        if meteor.top > HEIGHT:
+            meteors.remove(meteor)
+
     screen.fill(BLACK)
     pygame.draw.rect(screen, WHITE, player)
+    for meteor in meteors:
+        pygame.draw.rect(screen, RED, meteor)
 
     pygame.display.flip()
     clock.tick(60)
